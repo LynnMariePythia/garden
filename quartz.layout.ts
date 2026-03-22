aca09b5b69b39b0316c35883f13a8518defa4d0a
Lynn Marie DePippo <lynnmarie@pythiacapital.io>
2026-03-22 12:59:29 -0400

    Quartz sync: Mar 22, 2026, 12:59 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -13,7 +13,6 @@ export const sharedPageComponents: SharedLayout = {
       "RSS Feed": "/index.xml",
       "Subscribe": "https://buttondown.com/LynnMarie?tag=Garden",
       "Privacy Policy": "https://edge.pythiacapital.io/privacy",
-      "Terms of Use": "https://edge.pythiacapital.io/terms",
       "Disclaimer": "https://edge.pythiacapital.io/disclaimer",
       "Contact": "mailto:pythiacapital@gmail.com",
     },
